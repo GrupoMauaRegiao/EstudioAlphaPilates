0663adadb464e5b72120868c80beaf11cac7cf27
marcker <marckfree@gmail.com>
2014-03-18 17:08:01 -0300

    Adiciona página 'Blog'

---
+++ wordpress/wp-content/themes/estudio-alpha-pilates/blog.php
@@ -0,0 +1,48 @@
+      <section class="conteudo">
+        <article class="conteudo-textual">
+          <section class="titulo-pagina">
+            <a href="#materia" title="Atletas usam pilates para melhorar rendimento">Atletas usam pilates para melhorar rendimento</a>
+          </section>
+
+          <section class="imagem-artigo">
+            <img src="<?php bloginfo('template_url'); ?>/imagens/banner-post-blog.png" alt="" title="Imagem: atletas usam pilates para melhorar rendimento" />
+
+            <section class="efeito-hover" title="Imagem: atletas usam pilates para melhorar rendimento"></section>
+          </section>
+
+          <section class="data-publicacao">
+            <p title="Publicado em 13 de março de 2014 às 14h04">Publicado em 13 de março de 2014 às 14h04</p>
+          </section>
+
+          <section class="texto-conteudo">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, minus, odio, eligendi placeat iusto sit voluptatum nisi doloremque quam vero quaerat molestias. Necessitatibus, possimus totam voluptatum sunt aut dolorum perspiciatis? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, minus, odio, eligendi placeat iusto sit voluptatum nisi doloremque quam vero quaerat molestias. Necessitatibus, possimus totam voluptatum sunt aut dolorum perspiciatis?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, minus, odio, eligendi placeat iusto sit voluptatum nisi doloremque quam vero quaerat molestias. Necessitatibus, possimus totam voluptatum sunt aut dolorum perspiciatis? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, minus, odio, eligendi placeat iusto sit voluptatum nisi doloremque quam vero quaerat molestias. Necessitatibus, possimus totam voluptatum sunt aut dolorum perspiciatis?
+            </p>
+          </section>
+        </article>
+
+        <section class="veja-tambem">
+          <section class="header-veja-tambem">
+            <section class="balao">
+              <h2>Veja também</h2>
+            </section>
+            <section class="ponta-balao-veja-tambem"></section>
+          </section>
+
+          <section class="lista-artigos">
+            <ul>
+              <li><a href="#" title="Elas arrasam no pilates! Angélica leva dois pontos a frente">Elas arrasam no pilates! Angélica leva...</a></li>
+              <li><a href="#" title="Um artigo sobre pilates ainda é o mais lido">Um artigo sobre pilates ainda...</a></li>
+              <li><a href="#" title="Dificeis dilemas ao escolher a academia de pilates correta">Difíceis dilemas ao escolher a academia...</a></li>
+            </ul>
+          </section>
+
+          <section class="botao-ver-todos">
+            <a href="outros-posts.html" title="Ver todos os posts">Ver todos os posts</a>
+          </section>
+        </section>
+
+      </section>
--- wordpress/wp-content/themes/estudio-alpha-pilates/page.php
+++ wordpress/wp-content/themes/estudio-alpha-pilates/page.php
@@ -4,6 +4,8 @@ if (is_page("estudio")) {
   include "estudio.php";
 } elseif(is_page("pilates")) {
   include "pilates.php";
+} elseif (is_page("blog")) {
+  include "blog.php";
 }
 get_footer();
 ?>
