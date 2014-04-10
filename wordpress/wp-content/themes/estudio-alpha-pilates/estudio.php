3bfd3904fd1799b5f94f97363bfa183e97c417c5
marcker <marckfree@gmail.com>
2014-04-10 15:04:06 -0300

    Adiciona texto final a página 'Estúdio'

--- wordpress/wp-content/themes/estudio-alpha-pilates/estudio.php
+++ wordpress/wp-content/themes/estudio-alpha-pilates/estudio.php
@@ -1,16 +1,31 @@
       <section class="conteudo">
         <section class="conteudo-textual">
           <section class="titulo-pagina">
-            <h1>O estúdio</h1>
+            <h1>Falar de Pilates</h1>
+            <h2>Por Thaisa Paulino*</h2>
           </section>
 
           <section class="texto-conteudo">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, minus, odio, eligendi placeat iusto sit voluptatum nisi doloremque quam vero quaerat molestias. Necessitatibus, possimus totam voluptatum sunt aut dolorum perspiciatis? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, minus, odio, eligendi placeat iusto sit voluptatum nisi doloremque quam vero quaerat molestias. Necessitatibus, possimus totam voluptatum sunt aut dolorum perspiciatis?
+              A minha especialização em Pilates reflete a escolha profissional em me dedicar a estudar, cuidar e reabilitar o movimento  do corpo humano,  sou muita realizada com essa opção e  por isso  falo do Método com muita alegria.
             </p>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, minus, odio, eligendi placeat iusto sit voluptatum nisi doloremque quam vero quaerat molestias. Necessitatibus, possimus totam voluptatum sunt aut dolorum perspiciatis? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, minus, odio, eligendi placeat iusto sit voluptatum nisi doloremque quam vero quaerat molestias. Necessitatibus, possimus totam voluptatum sunt aut dolorum perspiciatis?
+              Sabemos que a prática de Pilates auxilia o indivíduo na melhora da sua saúde geral, os movimentos que trabalham de forma equilibrada alongamento e fortalecimento muscular, promovem um desenvolvimento equilibrado da musculatura respeitando à amplitude e as condições biomecânicas do corpo. Seja por razões de reabilitação ou condicionamento físico, uma nova postura é estabelecida, novos e saudáveis padrões de movimentos são sugeridos,  tensões e dores podem ser eliminadas. O método respeita cada fase da vida, trazendo harmonia nos níveis, físicos, mentais e emocionais.
             </p>
+            <p>
+              Atualmente tenho escutado a seguinte pergunta a respeito do método: "Pilates é tudo isso mesmo?" Bom, eu vejo da seguinte forma, pode ser muito benéfico ou não, mas o que determina essa diferença é o profissional e não o método em si. Em nosso país o Método Pilates nunca esteve tão em alta quanto agora. Muitas celebridades já declararam que praticam esta atividade, já contamos com representações das maiores empresas internacionais neste assunto no Brasil, vários instrutores vem se formando, e como  em outras modalidades alguns profissionais não têm a qualificação adequada para a aplicação do método.  Encontramos instrutores altamente qualificados  e outros com apenas conhecimentos básicos (mesmo assim atendendo qualquer tipo de cliente), infelizmente esse praticante não encontrará resultados de acordo com seu objetivo e/ou necessidade podendo até mesmo se lesionar, não através do método, mas sim pela deficiência do acompanhamento profissional. Então eu respondo,  o Pilates "pode" ser tudo isso sim!
+              
+            </p>
+            <p>
+              Nosso corpo é o melhor, mais preciso e PRECIOSO instrumento que temos para interagir com o mundo e transformar a realidade à nossa volta.  Além disso, um corpo bem equilibrado, flexível, ágil, forte e resistente nos proporciona  um grande prazer de viver, Por tanto, cuide de seu corpo e não se esqueça de identificar antes, com quem e onde está praticando esse maravilhoso método de treinamento, Bom Pilates e boas aulas!
+            </p>
+            <h2>
+              <strong>* Thaisa Paulino</strong><br>
+              Fisioterapeuta e técnica responsável do Estúdio Alpha<br>
+              Rua Campos Sales, 199 - Centro - Mauá - SP.<br>
+              Telefone: 11 3537-1122<br>
+              Duvidas? thapaulino@hotmail.com
+            </h2>
           </section>
 
           <section class="ilustracao"></section>
