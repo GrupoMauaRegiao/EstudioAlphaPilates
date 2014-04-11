38ad3ab5f93118d7ebf5c922b5d2631c28ff2990
marcker <marckfree@gmail.com>
2014-04-11 08:32:24 -0300

    Adiciona texto final a página 'Pilates'

--- wordpress/wp-content/themes/estudio-alpha-pilates/pilates.php
+++ wordpress/wp-content/themes/estudio-alpha-pilates/pilates.php
@@ -2,14 +2,42 @@
         <section class="conteudo-textual">
           <section class="titulo-pagina">
             <h1>Joseph Pilates</h1>
+            <h2>Por Thaisa Paulino &#8212; CREFITO-3/134071-F</h2>
           </section>
 
           <section class="texto-conteudo">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, minus, odio, eligendi placeat iusto sit voluptatum nisi doloremque quam vero quaerat molestias. Necessitatibus, possimus totam voluptatum sunt aut dolorum perspiciatis? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, minus, odio, eligendi placeat iusto sit voluptatum nisi doloremque quam vero quaerat molestias. Necessitatibus, possimus totam voluptatum sunt aut dolorum perspiciatis?
+              PILATES é um método de atividade física elaborado pelo alemão Joseph Pilates em 1920. Pensando no princípio de "mente sã e corpo são", Joseph criou mais de 500 exercícios baseados em seis princípios básicos: respiração, concentração, controle, alinhamento, centralização e integração de movimentos. Bem executado e orientado, não traz impactos nocivos para as articulações, ligamentos e musculatura, pelo contrário, melhora a postura, reduz o stress, alonga e tonifica a musculatura sem exageros. Ou seja, praticando Pilates você melhora a qualidade de vida, a saúde e a estética.
+            </p>
+            <ul>
+              <li><strong>Quais os benefícios do pilates?</strong></li>
+            </ul>
+            <p>
+              Pilates exercita vários grupos musculares simultaneamente, trabalha a reeducação do movimento e proporciona às pessoas uma compreensão de seus corpos, assim podendo usá-los de forma mais eficiente, melhorando a postura nas atividades de vida diária e profissional, deixando o corpo menos vulnerável a lesões.
             </p>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, minus, odio, eligendi placeat iusto sit voluptatum nisi doloremque quam vero quaerat molestias. Necessitatibus, possimus totam voluptatum sunt aut dolorum perspiciatis? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, minus, odio, eligendi placeat iusto sit voluptatum nisi doloremque quam vero quatradiçãoerat molestias. Necessitatibus, possimus totam voluptatum sunt aut dolorum perspiciatis?
+              Com as aulas, o aluno percebe melhora no stress, sente-se mais revigorado e ágil, ganha aumento de flexibilidade muscular, "barriga mais definida", autocontrole emocional, ótima postura, articulações mais saudáveis, coordenação neuromuscular e tonifica a musculatura de forma equilibrada, resultando em um corpo mais harmonioso melhorando a auto-estima e deixando uma mente alerta e sadia.
+            </p>
+            <ul>
+              <li><strong>Quem pode praticar Pilates?</strong></li>
+            </ul>
+            <p>
+              Qualquer pessoa a partir dos 6 anos de idade em qualquer nível de condicionamento ou com qualquer tipo de limitação funcional.
+            </p>
+            <ul>
+              <li><strong>Existe alguma contra indicação?</strong></li>
+            </ul>
+            <p>
+              Não, o que existe é algum exercício contra indicado para determinado tipo de patologia. Por isso a importância de se procurar um profissional especializado, pois só ele pode fazer uma avaliação física e elaborar o programa de treinamento mais adequado às necessidades do aluno.
+            </p>
+            <ul>
+              <li><strong>Pilates é apenas mais uma moda passageira?</strong></li>
+            </ul>
+            <p>
+              A resposta para a pergunta é simples: não. E a razão é mais simples ainda: porque realmente funciona.
+            </p>
+            <p>
+              Joseph Pilates sempre disse que seu método estava 50 anos a frente de seu tempo, e para sustentar sua teoria é só observar o crescimento e popularidade atual do Pilates. Por exemplo, há dez anos 5000 pessoas faziam exercícios de Pilates em todo o mundo, o número agora é de 5 milhões só nos EUA! Agora eu te pergunto, com tantas pessoas praticando por tanto tempo, será que o Método Pilates realmente funciona?
             </p>
           </section>
 
